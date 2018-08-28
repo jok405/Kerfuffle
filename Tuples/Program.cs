80066aaa8c72d722d8fe177eec048e892a01d95b
jok405 <john.y.kim84@gmail.com>
2018-08-28 15:01:32 -0400

    initial upload

---
+++ Tuples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runner runner = new Runner();
+            runner.Run();
+        }
+    }
+    public class Runner
+    {
+        public void Run()
+        {
+            var time = GetTime();
+        }
+        public (int, int, int) GetTime()
+        {
+            return (1, 30, 40); // tuple literal
+        }
+    }
+}
